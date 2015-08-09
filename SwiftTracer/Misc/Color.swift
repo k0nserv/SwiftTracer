afe950b155c05a460f5a3ee12e75bf22df603654
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-09 22:54:43 +0200

    [Material] Use a proper struct for color

---
+++ SwiftTracer/Misc/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  SwiftTracer
+//
+//  Created by Hugo Tunius on 09/08/15.
+//  Copyright © 2015 Hugo Tunius. All rights reserved.
+//
+
+import Foundation
+
+struct Color {
+    let r: Double
+    let g: Double
+    let b: Double
+}
+
+func *(left: Color, right: Double) -> Color {
+    return Color(r: left.r * right, g: left.g * right, b: left.b * right)
+}
--- SwiftTracer/Misc/Material.swift
+++ SwiftTracer/Misc/Material.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 struct Material {
-    let color: Vector
+    let color: Color
 }
