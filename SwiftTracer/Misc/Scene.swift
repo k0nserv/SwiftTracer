eaa0cdba980873742f0e887847884701796c9fa3
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-09 22:56:01 +0200

    [Scene] Implement a struct

---
+++ SwiftTracer/Misc/Scene.swift
@@ -0,0 +1,14 @@
+//
+//  Scene.swift
+//  SwiftTracer
+//
+//  Created by Hugo Tunius on 09/08/15.
+//  Copyright © 2015 Hugo Tunius. All rights reserved.
+//
+
+import Foundation
+
+
+struct Scene {
+    let objects: [Shape]
+}
