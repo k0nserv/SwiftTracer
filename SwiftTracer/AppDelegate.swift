6b1d9b23ae765543de0a0838d864db70f617e616
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-09 18:22:43 +0200

    Initial Commit

---
+++ SwiftTracer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftTracer
+//
+//  Created by Hugo Tunius on 09/08/15.
+//  Copyright © 2015 Hugo Tunius. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
