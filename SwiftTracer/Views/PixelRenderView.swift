9d5771aabea4b55f7e49f566566172a51c41579a
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-17 00:59:11 +0200

    Revert "[PixelRenderView] Switch to OpenGL texture based"
    This reverts commit 953193206e7fd3dfe01f8bdc644f664262fb39e7.

--- SwiftTracer/Views/PixelRenderView.swift
+++ SwiftTracer/Views/PixelRenderView.swift
@@ -7,39 +7,21 @@
 //
 
 import Cocoa
-import GLKit
 
 class PixelRenderView: NSView {
     var pixels: [[Color]] = []
-class PixelRenderView: NSOpenGLView {
-    var textureID: GLuint = 0
-    var pixels: [[Color]] = [] {
-        didSet {
-            var result: [Double] = []
-            for var x = 0; x < pixels.count; x++ {
-                for var y = 0; y < pixels[x].count; y++ {
-                    let color = pixels[x][y]
 
-                    result.append(color.r)
-                    result.append(color.g)
-                    result.append(color.b)
-                }
-            }
-            glTexImage2D(UInt32(GL_TEXTURE_2D), GLint(0), GLint(GL_RGB), GLsizei(pixels.count), GLsizei(pixels[0].count), GLint(0), UInt32(GL_RGB), UInt32(GL_DOUBLE), &result)
-        }
-    }
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+        let rep = NSBitmapImageRep(focusedViewRect: dirtyRect)
 
-    override func prepareOpenGL() {
-        super.prepareOpenGL()
-        glClearColor(0.0, 0.0, 0.0, 1.0)
-        glGenTextures(1, &textureID)
-
-        glBindTexture(UInt32(GL_TEXTURE_2D), textureID)
-        glTexParameteri(UInt32(GL_TEXTURE_2D), UInt32(GL_TEXTURE_WRAP_S), GL_CLAMP_TO_BORDER)
-        glTexParameteri(UInt32(GL_TEXTURE_2D), UInt32(GL_TEXTURE_WRAP_T), GL_CLAMP_TO_BORDER)
+        for var x = 0; x < pixels.count; ++x {
+            for var y = 0; y < pixels[x].count; ++y {
+                let pixel = pixels[x][y]
+                var pixelValues = [Int(pixel.r * 255), Int(pixel.g * 255), Int(pixel.b * 255), 255]
+                rep!.setPixel(&pixelValues, atX: x, y: y)
             }
         }
-    }
 
         rep!.drawInRect(dirtyRect)
     }    
