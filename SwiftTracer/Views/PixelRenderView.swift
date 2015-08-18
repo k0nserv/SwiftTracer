f78b47832940338c49fd22b9742ffee18a083846
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-18 15:36:31 +0200

    [PixelRenderView] Improve display performance
    Why:
    
    Switch to setting NSBitMapImageRep's pixel data by
    copying the full buffer instead of looping over pixels
    and manually setting each one. Still a bit too slow, but
    a big improvement over the previous solution

--- SwiftTracer/Views/PixelRenderView.swift
+++ SwiftTracer/Views/PixelRenderView.swift
@@ -13,16 +13,44 @@ class PixelRenderView: NSView {
 
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
-        let rep = NSBitmapImageRep(focusedViewRect: dirtyRect)
+        if pixels.count == 0 || pixels[0].count == 0 {
+            return
+        }
 
-        for var x = 0; x < pixels.count; ++x {
-            for var y = 0; y < pixels[x].count; ++y {
+        let width = pixels.count
+        let height = pixels[0].count
+
+        let rep = NSBitmapImageRep(bitmapDataPlanes: nil,
+                                         pixelsWide: width,
+                                         pixelsHigh: height,
+                                      bitsPerSample: 8,
+                                    samplesPerPixel: 3,
+                                           hasAlpha: false,
+                                           isPlanar: false,
+                                     colorSpaceName: NSDeviceRGBColorSpace,
+                                        bytesPerRow: width * 3,
+                                       bitsPerPixel: 24)
+        guard let r = rep else {
+            return
+        }
+
+        let pixelData = r.bitmapData
+        NSLog("Width: \(width)")
+        NSLog("Height: \(height)")
+        NSLog("Bytes per row \(r.bytesPerRow)")
+
+        for var y = 0; y < height; ++y {
+            for var x = 0; x < width; ++x {
                 let pixel = pixels[x][y]
-                var pixelValues = [Int(pixel.r * 255), Int(pixel.g * 255), Int(pixel.b * 255), 255]
-                rep!.setPixel(&pixelValues, atX: x, y: y)
+                let index = y * r.bytesPerRow + x * 3
+                pixelData[index + 0] = UInt8(pixel.r * 255)
+                pixelData[index + 1] = UInt8(pixel.g * 255)
+                pixelData[index + 2] = UInt8(pixel.b * 255)
             }
         }
 
-        rep!.drawInRect(dirtyRect)
+
+
+        r.drawInRect(dirtyRect)
     }    
 }
