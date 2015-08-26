e432eb8a2addb0442efce53f1ba9c510c7c1397a
Hugo Tunius <hugo.tunius@gmail.com>
2015-08-26 21:05:46 +0100

    Eliminate redundant calculations in Camera
    Why:
    
    Some calculations with the fov angle were redundantaly
    performed for each ray spawned

--- SwiftTracer/Misc/Camera.swift
+++ SwiftTracer/Misc/Camera.swift
@@ -11,23 +11,27 @@ import Foundation
 
 struct Camera {
     let fov: Double
-    let verticalFov: Double
     let width: Int
     let height: Int
     let aspectRatio: Double
+
+    private let x0: Double
+    private let y0: Double
 
     init(fov: Double, width: Int, height: Int) {
         self.fov = fov
         self.width = width
         self.height = height
         self.aspectRatio = Double(height) / Double(width)
-        self.verticalFov = self.fov * aspectRatio
+        let verticalFov = self.fov * aspectRatio
+        self.x0 = sin(fov * 0.5)
+        self.y0 = sin(verticalFov * 0.5)
     }
 
     func createRay(x x: Int, y: Int) -> Ray {
         let px = Double(x) * 2 / Double(width) - 1
         let py = Double(y) * 2 / Double(height) - 1
-        let direction = Vector(x: px * sin(fov * 0.5), y: py * sin(verticalFov * 0.5), z: 1.0)
+        let direction = Vector(x: px * x0, y: py * y0, z: 1.0)
 
         return Ray(origin: Vector(x: 0, y: 0, z: 0), direction: direction.normalize())
     }
